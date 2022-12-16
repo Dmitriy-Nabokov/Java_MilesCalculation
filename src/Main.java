68d4052b59bff7b03a54520f1b49e0342ee0b479
Neto test <loktionov.dv@mail.ru>
2022-12-16 13:17:09 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+                int cost = 7_135;
+                int mile = 20;
+
+                System.out.println("Цена билета: " + cost + " руб.");
+
+        System.out.println("Начисленные мили за купленный билет: " + (cost/mile));
+    }
+}
